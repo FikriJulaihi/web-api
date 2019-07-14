774a759a486f4fb1447f7e583be864298deef4a4
abdullah fikri <41502211+FikriJulaihi@users.noreply.github.com>
2019-07-14 21:09:40 +0800

    Add files via upload

---
+++ server/movie.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const db = 'mongodb+srv://fikri:testabc@inti-db-ijqg8.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(error => {
+    console.log('Mongoose connetion error: ', error);
+  });
+
+const schema = mongoose.Schema({
+  title: { type: String },
+  year: { type: String },
+  genre: { type: String },
+  actors: { type: String },
+  plot: { type: String },
+  poster: { type: String }
+});
+
+const Movie = mongoose.model('Movie', schema, 'movieCollection');
+
+module.exports = Movie;
